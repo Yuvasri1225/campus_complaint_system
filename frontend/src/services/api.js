e6770b992a5c4ea6596a4ad46bab46e68fe71a1b
Yuvasri1225 <23cseb30yuvasri@gmail.com>
2026-04-05 21:34:26 +0530

    update API URL for production

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000"
+  baseURL: "https://campus-complaint-system-3iqb.onrender.com"
 });
 
 export const createComplaint = (data) => API.post("/complaints", data);
